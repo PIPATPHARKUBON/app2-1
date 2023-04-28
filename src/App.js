8bdad5e319042c80fb47d102fb43f90d84fd0525
PIPATPHARKUBON <you@example.com265345221017-st@rmutsb.ac.th>
2023-04-28 11:44:56 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,19 @@ import {Myimage, Myabout,Mydata} from './func-mydata'
 import MsgBox from './func-props'
 import Caledar from './class-components'
 import { Navber } from './func-components'
-
-
+import MsgBox2 from './class-props'
+import List from './list-comp'
 
 
 
 function App() {
 
-  return   [<Navber/>,<Myabout/>,<Myimage/>,<Mydata/>,<Caledar/>]
+  return (<List/>)
+
+ // return   [<Navber/>,<Myabout/>,<Myimage/>,<Mydata/>,<Caledar/>,<MsgBox/>,<MsgBox2/>]
   
-}
-
-
 /*return(
-  <MsgBox 
+  <MsgBox2 
   text="Useing props in function component"
   color="red"
   bgColor="#ccc"
@@ -26,5 +25,6 @@ function App() {
   />
 )*/
 
+}
 
 export default App;
